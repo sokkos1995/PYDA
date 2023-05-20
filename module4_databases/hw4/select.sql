3e7bfc23565ebf8643867fcc161392d051a45017
Konstantin Sokolov <k.sokolov@netology.ru>
2023-05-20 19:47:31 +0300

    fixed mistakes in queries hw4

--- module4_databases/hw4/select.sql
+++ module4_databases/hw4/select.sql
@@ -35,11 +35,15 @@ join album a on t.album_id = a.id
 group by a."name"  ;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году.
-select a.pseudo_name 
-from artists a 
-join artists_albums aa on a.id = aa.artist_id 
-left join album a2 on a2.id = aa.album_id and date_part('year', a2.created_at) = 2020
-where a2.created_at is null;
+SELECT a.pseudo_name  /* Получаем имена исполнителей */
+FROM artists a  /* Из таблицы исполнителей */
+WHERE a.pseudo_name NOT IN ( /* Где имя исполнителя не входит в вложенную выборку */
+    SELECT a.pseudo_name /* Получаем имена исполнителей */
+    FROM artists a /* Из таблицы исполнителей */
+    JOIN artists_albums aa ON a.id = aa.artist_id  /* Объединяем с промежуточной таблицей */
+    JOIN album a2 ON a2.id = aa.album_id /* Объединяем с таблицей альбомов */
+    WHERE date_part('year', a2.created_at) = 2020 /* Где год альбома равен 2020 */
+);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 select c."name" 
@@ -52,12 +56,12 @@ where a.pseudo_name = 'Eminem'
 group by 1;
 
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
-select a."name"  
-from album a 
-join artists_albums aa on a.id = aa.album_id 
-join genres_artists ga on aa.artist_id = ga.artist_id 
-group by a."name" 
-having count(genre_id) > 1;
+SELECT distinct a."name"  /* Получаем ТОЛЬКО уникальные имена альбомов. Другие данные в выводе не нужны */
+FROM album a /* Из таблицы альбомов */
+JOIN artists_albums aa on a.id = aa.album_id /* Объединяем альбомы с промежуточной таблицей между альбомами и исполнителями */
+JOIN genres_artists ga on aa.artist_id = ga.artist_id  /* Объединяем промежуточную таблицу выше с промежуточной таблицей между исполнителями и жанрами */
+GROUP BY a."name", aa.artist_id  /* Группируем по айди альбомов и айди исполнителей из промежуточной таблицы между исполнителями и жанрами */
+HAVING COUNT(genre_id) > 1; /* Где количество id жанров из промежуточной таблицы больше 1 */
 
 --Наименования треков, которые не входят в сборники.
 select t."name" 
@@ -85,3 +89,14 @@ select a."name"
 from album_track_count atc
 join album a on a.id = atc.album_id
 where track_counter = (select min(track_counter) from album_track_count);
+
+--альтернативный вариант
+SELECT a."name"  /* Названия альбомов */
+FROM album a JOIN track t ON a.id = t.album_id /* Объединяем альбомы и треки */
+GROUP BY a."name" /* Группируем по айди альбомов */
+HAVING COUNT(track_id) = ( /* Где количество треков равно вложенному запросу, в котором получаем наименьшее количество треков в одном альбоме */
+    SELECT COUNT(track_id) FROM track t /* Получаем поличество айди треков из таблицы треков*/
+    GROUP BY album_id /* Группируем по айди альбомов */
+    ORDER BY 1 /* Сортируем по первому столбцу */
+    LIMIT 1 /* Получаем первую запись */
+);
